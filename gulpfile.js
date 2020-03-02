0448fd3f1f26967968d4b259ce1e2954a9c458fa
SK.CODER <skorniychyk@gmail.com>
2020-03-02 12:30:27 +0200

    added mincss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 function style () {
     return gulp.src('./scss/**/*.scss')
@@ -19,5 +21,13 @@ function watch () {
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
+function minicss () {
+      return gulp.src('./css/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css'))
+}
+
 exports.style = style;
 exports.watch = watch;
+exports.minicss = minicss;

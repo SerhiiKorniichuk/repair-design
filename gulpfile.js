e4439a7b34b6709d29374f59d1f969852ffcbc36
Serhii_Korniichuk <skorniychyk@gmail.com>
2020-03-03 17:15:14 +0200

    add min css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const minifyCSS = require('gulp-minify-css');
+
+function minicss () {
+  return gulp.src('src/css/*.css')
+      .pipe(minifyCSS())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('dist/css'))
 }
 
-exports.default = defaultTask
+function watch () {
+  browserSync.init({
+    server: {
+      baseDir: './src/'
+    }
+  })
+  gulp.watch('./src/css/*.css').on('change', browserSync.reload);
+  gulp.watch('./src/*.html').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
+exports.minicss = minicss;

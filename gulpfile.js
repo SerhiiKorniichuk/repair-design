50323015ec8934317d772931e0d6808e191b78e6
SK.CODER <skorniychyk@gmail.com>
2020-03-03 11:46:05 +0200

    created new repositories files

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,6 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
-
-function style () {
-    return gulp.src('./scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream())
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
 }
 
-function watch () {
-    browserSync.init({
-      server: {
-        baseDir: './'
-      }
-    })
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-}
-
-function minicss () {
-      return gulp.src('./css/**/*.css')
-      .pipe(cssmin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./css'))
-}
-
-exports.style = style;
-exports.watch = watch;
-exports.minicss = minicss;
+exports.default = defaultTask

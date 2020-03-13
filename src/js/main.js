a37492cf508ce5da8e427e1bedf9f2c5a3f36eba
Serhii_Korniichuk <skorniychyk@gmail.com>
2020-03-13 10:37:18 +0200

    refact: js => jQuery and add button go up

--- src/js/main.js
+++ src/js/main.js
@@ -40,9 +40,25 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
-  closeModal.on('keydown', function () {
-    if (event.code == 'Escape') {
-      modal.removeClass('modal--visible');
-    }
+  // closeModal.on('keydown', function () {
+  //   if (event.code == 'Escape') {
+  //     modal.removeClass('modal--visible');
+  //   }
+  // });
+});
+
+var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+var delay = 1000; // Задержка прокрутки
+$(document).ready(function() {
+  $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+    /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+    if ($(this).scrollTop() > top_show) $('.button-go-up ').fadeIn();
+    else $('.button-go-up ').fadeOut();
+  });
+  $('.button-go-up ').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+    /* Плавная прокрутка наверх */
+    $('body, html').animate({
+      scrollTop: 0
+    }, delay);
   });
 });

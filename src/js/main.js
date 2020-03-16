80f08e2a54fda2a2e884fbcb3fcfc87463f98e86
Serhii_Korniichuk <skorniychyk@gmail.com>
2020-03-16 21:26:56 +0200

    feat: add jquery keyframes animation

--- src/js/main.js
+++ src/js/main.js
@@ -87,24 +87,66 @@ $(document).ready(function () {
   bulletsProjects.css('left', prevProjects.width() + 20);
 
 
-
-
-
-
-  var top_show = 500; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
-  var delay = 1000; // Задержка прокрутки
-  $(window).scroll(function () { // При прокрутке попадаем в эту функцию
-    /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
-    if ($(this).scrollTop() > top_show) $('.button-go-up ').fadeIn();
-    else $('.button-go-up ').fadeOut();
+  var el1 = $('.projects__row');
+  var el2 = $('.control__container');
+  var el3 = $('.types__container');
+  var el4 = $('.types_card1');
+  var el5 = $('.types_card2');
+  var el6 = $('.types_card3');
+  var el7 = $('.design__container');
+  var el8 = $('.design__card1');
+  var el9 = $('.design__card2');
+  var el10 = $('.design__card3');
+  $(window).scroll(function(){
+    if ( $(this).scrollTop() > el1.offset().top - 980) {
+        el1.addClass('animkey');
+    }
+    if ( $(this).scrollTop() > el2.offset().top - 980) {
+        el2.addClass('animkey');
+    }
+    if ( $(this).scrollTop() > el3.offset().top - 980) {
+        el3.addClass('animkey');
+    }
+    if ( $(this).scrollTop() > el4.offset().top - 980) {
+      el4.addClass('animkeyForCards1');
+    }
+    if ( $(this).scrollTop() > el5.offset().top - 980) {
+      el5.addClass('animkeyForCards2');
+    }
+    if ( $(this).scrollTop() > el6.offset().top - 980) {
+      el6.addClass('animkeyForCards3');
+    }
+    if ( $(this).scrollTop() > el7.offset().top - 980) {
+      el7.addClass('animkey');
+    }
+    if ( $(this).scrollTop() > el8.offset().top - 980) {
+      el8.addClass('animkeyForCards1');
+    }
+    if ( $(this).scrollTop() > el9.offset().top - 980) {
+      el9.addClass('animkeyForCards2');
+    }
+    if ( $(this).scrollTop() > el10.offset().top - 980) {
+      el10.addClass('animkeyForCards3');
+    }
   });
-  $('.button-go-up ').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
-    /* Плавная прокрутка наверх */
-    $('body, html').animate({
-      scrollTop: 0
-    }, delay);
-  });
+
+  
 
 
-  new WOW().init();
+  // var top_show = 500; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  // var delay = 1000; // Задержка прокрутки
+  // $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+  //   /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+  //   if ($(this).scrollTop() > top_show) $('.button-go-up ').fadeIn();
+  //   else $('.button-go-up ').fadeOut();
+  // });
+  // $('.button-go-up ').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+  //   /* Плавная прокрутка наверх */
+  //   $('body, html').animate({
+  //     scrollTop: 0
+  //   }, delay);
+  // });
+
+
+  // new WOW().init();
 });

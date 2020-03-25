29d7c2f8957548eb97c786f99d30286746af3525
Serhii_Korniichuk <skorniychyk@gmail.com>
2020-03-25 13:53:06 +0200

    fix: clients cards on IE 11

--- dist/js/main-debug.js
+++ dist/js/main-debug.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
   var player;
   $('.video__play-button').on('click', function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-      // height: '360',
+      height: '100%',
       width: '100%',
       videoId: '_Qhqb8DrTNA',
       events: {
